479e4707bb291cba6617a07481d726c5902c6fd4
nishant <nishantkujur@Nishants-MacBook-Pro.local>
2026-06-03 16:06:28 +0530

    Added GitDemo1 class

---
+++ src/nishantGitTest/GitDemo1.java
@@ -0,0 +1,11 @@
+package nishantGitTest;
+
+public class GitDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi Nishant");
+	}
+
+}
