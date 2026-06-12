ebf36a4034dae337c765683242e2e04bf5ee7213
nishant <nishantkujur@Nishants-MacBook-Pro.local>
2026-06-12 20:00:27 +0530

    from Demo

---
+++ src/nishantGitTest/gitDemo2.java
@@ -0,0 +1,41 @@
+package nishantGitTest;
+
+public class gitDemo2 {
+	
+	
+	
+public void testCommands1(){
+		
+		System.out.println("T1");
+		
+	}
+	
+	public void testCommands2(){
+		
+		System.out.println("T2");
+		
+	}
+	
+	public void testCommands3(){
+		
+		System.out.println("T3");
+		
+	}
+	
+	public void testCommands4(){
+		
+		System.out.println("Test4");
+		
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+	
+	
+		
+	}
+
+}
