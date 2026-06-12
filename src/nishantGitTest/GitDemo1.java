c389518a014683d448749173ac347a8d47af4688
nishant <nishantkujur@Nishants-MacBook-Pro.local>
2026-06-12 17:59:01 +0530

    committed

--- src/nishantGitTest/GitDemo1.java
+++ src/nishantGitTest/GitDemo1.java
@@ -2,6 +2,35 @@ package nishantGitTest;
 
 public class GitDemo1 {
 
+	
+	public void codeChangeRam()
+	{
+		System.out.println("1st code change");
+		
+		}
+	
+	
+	public void codeChangeSam()
+	{
+		System.out.println("2nd code change");
+		
+		}
+	
+	public void uiAutomation(){
+		
+		System.out.println("UI Testing");
+		
+	}
+	
+	
+public void mobileAutomation(){
+		
+		System.out.println("Mobile Testing");
+		
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
